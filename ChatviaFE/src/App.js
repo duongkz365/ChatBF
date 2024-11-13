a08036830ac398225945b57386f5220e8858aec1
duongkz365 <dinhky2468@example.com>
2024-11-13 12:02:17 +0700

    configure signalR Provider

--- ChatviaFE/src/App.js
+++ ChatviaFE/src/App.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
-import Routes from './routes';
+import React, { useEffect } from "react";
+import Routes from "./routes";
 
 //Import Scss
 import "./assets/scss/themes.scss";
 
 //fackbackend
-import fakeBackend from './helpers/fake-backend';
-import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
+import fakeBackend from "./helpers/fake-backend";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { SignalRProvider } from "./routes/SignalRContext";
 
 // //Firebase helper
 // import { initFirebaseBackend } from "./helpers/firebase";
@@ -25,26 +26,30 @@ fakeBackend();
 // 	appId: process.env.REACT_APP_APPID,
 // 	measurementId: process.env.REACT_APP_MEASUREMENTID,
 // };
-  
+
 // // init firebase backend
 // initFirebaseBackend(firebaseConfig);
 
 function App() {
-
   const selectLayoutProperties = createSelector(
     (state) => state.Layout,
     (layout) => ({
       layoutMode: layout.layoutMode,
     })
   );
-  
+
   const { layoutMode } = useSelector(selectLayoutProperties);
 
-useEffect(() => {
-  layoutMode && localStorage.setItem("layoutMode",layoutMode);
-}, [layoutMode])
+  useEffect(() => {
+    layoutMode && localStorage.setItem("layoutMode", layoutMode);
+  }, [layoutMode]);
 
-  return <Routes />;
-};
+  return (
+    <SignalRProvider>
+      <Routes />
+    </SignalRProvider>
+  );
+  // return <Routes />;
+}
 
 export default App;
--- ChatviaFE/src/pages/dashboard/index.js
+++ ChatviaFE/src/pages/dashboard/index.js
@@ -1,131 +1,181 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 // Import Components
 import ChatLeftSidebar from "./ChatLeftSidebar";
 import UserChat from "./UserChat/index";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchContactRequest } from '../../redux/contact/action';
-import { fetchConversationRequest } from '../../redux/conversation/action';
-import { fetchMessageRequest } from '../../redux/message/actions';
-import { fetchProfileRequest } from '../../redux/profile/actions';
-import { loginProfileRequest } from '../../redux/profile/actions';
-import { loginUser } from '../../redux/actions';
-import { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { fetchContactRequest } from "../../redux/contact/action";
+import { fetchConversationRequest } from "../../redux/conversation/action";
+import { fetchMessageRequest } from "../../redux/message/actions";
+import { fetchProfileRequest } from "../../redux/profile/actions";
+import { loginProfileRequest } from "../../redux/profile/actions";
+import { loginUser } from "../../redux/actions";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
+import { HubConnectionBuilder } from "@microsoft/signalr";
+import { sendValueVideoCall } from "../../redux/control/actions";
 
-
-import { HubConnectionBuilder } from '@microsoft/signalr';
-import { sendValueVideoCall } from '../../redux/control/actions';
+import { useSignalR } from "../../routes/SignalRContext";
 const Index = () => {
-    const users = useSelector((state) => state.Chat.users);
+  const users = useSelector((state) => state.Chat.users);
 
-    useEffect(() => {
-        document.title = "Dashboard | Chatvia";
-    }, []);
+  useEffect(() => {
+    document.title = "Dashboard | Chatvia";
+  }, []);
 
-    const dispatch = useDispatch();
-   
+  const dispatch = useDispatch();
 
-    
-    const [connection, setConnection] = useState(null);
+  const [connection, setConnection] = useState(null);
 
-    useEffect(() => {
-        dispatch(fetchContactRequest());
-        dispatch(fetchMessageRequest());
-        dispatch(fetchProfileRequest());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchContactRequest());
+    dispatch(fetchMessageRequest());
+    dispatch(fetchProfileRequest());
+  }, [dispatch]);
 
-    const userId = useSelector((state)=> state.Profile?.profile)?.userId;
-    console.log(userId)
+  const userId = useSelector((state) => state.Profile?.profile)?.userId;
 
-    
-    useEffect(() => {
+  console.log("id: ", userId);
+  const { sendUserId, onEvent } = useSignalR();
 
-        const newConnection = new HubConnectionBuilder()
-            .withUrl("https://localhost:7098/chathub") // Đảm bảo URL trùng với backend của bạn
-            .withAutomaticReconnect()
-            .build();
+  useEffect(() => {
+    onEvent("ReceiveMessage", (msg) => {
+      console.log("Message Received:", msg);
+      dispatch(fetchMessageRequest());
+    });
+    return () => {
+      if (onEvent) onEvent("ReceiveMessage", () => {});
+    };
+  }, [onEvent, dispatch]);
 
-        setConnection(newConnection);
-    }, []);
+  useEffect(() => {
+    onEvent("RequestCall", (message) => {
 
-    useEffect(() => {
-        if (connection && userId) {
-            // Định nghĩa các sự kiện nhận tin nhắn từ server
+        console.log("Có cuộc gọi")
+      if (message.call.receiver === userId) {
+        localStorage.setItem("call", JSON.stringify(message));
+        window.location.href = "/call-pending";
+      }
+    });
+    return () => {
+      if (onEvent) onEvent("RequestCall", () => {});
+    };
+  }, [onEvent]);
 
-            // Bắt đầu kết nối và gửi userId khi kết nối thành công
-            connection.start()
-                .then(async () => {
-                    console.log("Kết nối SignalR thành công!");
+  useEffect(() => {
+    onEvent("AcceptCall", (message) => {
+      if (message.call.caller === userId) {
+        localStorage.setItem("call", JSON.stringify(message));
+        window.location.href = "/video-call";
+      }
+    });
+    return () => {
+      if (onEvent) onEvent("AcceptCall", () => {});
+    };
+  }, [onEvent]);
 
-                    // Lấy Connection ID từ server
-                    const connectionId = await connection.invoke("GetConnectionId");
-                    console.log("Connection ID của tôi:", connectionId);
+  useEffect(() => {
+    onEvent("CancelCall", (message) => {
+      if (message.call.caller === userId) {
+        console.log("Đã từ chối");
+        localStorage.setItem("call", "");
+        window.location.href = "/";
+      }
+    });
+    return () => {
+      if (onEvent) onEvent("CancelCall", () => {});
+    };
+  }, [onEvent]);
 
-                    // Gửi userId đến server để lưu kèm Connection ID
-                    await connection.invoke("StoreUserId", userId);
+  useEffect(() => {
+    if (userId) {
+      sendUserId(userId);
+      console.log("send ", userId);
+    }
+  }, [userId, sendUserId]);
 
+  // useEffect(() => {
 
-                    // Lang nghe cac su kien
+  //     const newConnection = new HubConnectionBuilder()
+  //         .withUrl("https://localhost:7098/chathub") // Đảm bảo URL trùng với backend của bạn
+  //         .withAutomaticReconnect()
+  //         .build();
 
+  //     setConnection(newConnection);
+  // }, []);
 
-                    connection.on("ReceiveMessage", (message) => {
-                        console.log("Nhận tin nhắn từ server:", message);
-                        // Xử lý tin nhắn nhận được (ví dụ: dispatch đến Redux hoặc cập nhật state)
-                        dispatch(fetchMessageRequest());
-                    });
+  // useEffect(() => {
+  //     if (connection && userId) {
+  //         // Định nghĩa các sự kiện nhận tin nhắn từ server
 
-                    connection.on("AcceptCall", (message) => {
-                        console.log("Nhận tin nhắn từ server:", message);
-                        // Xử lý tin nhắn nhận được (ví dụ: dispatch đến Redux hoặc cập nhật state)
-                        // dispatch(fetchMessageRequest());
-                        if(message.call.caller === userId){
+  //         // Bắt đầu kết nối và gửi userId khi kết nối thành công
+  //         connection.start()
+  //             .then(async () => {
+  //                 console.log("Kết nối SignalR thành công!");
 
-                            console.log("đã chấp nhận")
-                            localStorage.setItem('call',JSON.stringify(message));
-                            window.location.href = '/video-call';
-                        }
-                    });
+  //                 // Lấy Connection ID từ server
+  //                 const connectionId = await connection.invoke("GetConnectionId");
+  //                 console.log("Connection ID của tôi:", connectionId);
 
-                    connection.on("CancelCall", (message) => {
-                        console.log("Nhận tin nhắn từ server:", message);
-                        if(message.call.caller === userId){
+  //                 // Gửi userId đến server để lưu kèm Connection ID
+  //                 await connection.invoke("StoreUserId", userId);
 
-                            console.log("Đã từ chối")
-                            localStorage.setItem('call',"");
-                            window.location.href = '/';
-                        }
-                    });
+  //                 // Lang nghe cac su kien
 
-                    connection.on("RequestCall", (message) => {
-                        if(message.call.receiver === userId){
-                            localStorage.setItem('call',JSON.stringify(message));
-                            window.location.href = '/call-pending';
-                        }
-                    });
+  //                 connection.on("ReceiveMessage", (message) => {
+  //                     console.log("Nhận tin nhắn từ server:", message);
+  //                     // Xử lý tin nhắn nhận được (ví dụ: dispatch đến Redux hoặc cập nhật state)
+  //                     dispatch(fetchMessageRequest());
+  //                 });
 
-                })
-                .catch((err) => console.error("Kết nối SignalR thất bại:", err));
+  //                 connection.on("AcceptCall", (message) => {
+  //                     console.log("Nhận tin nhắn từ server:", message);
+  //                     // Xử lý tin nhắn nhận được (ví dụ: dispatch đến Redux hoặc cập nhật state)
+  //                     // dispatch(fetchMessageRequest());
+  //                     if(message.call.caller === userId){
 
-            // Xử lý ngắt kết nối khi component unmount
-            return () => {
-                connection.stop().then(() => console.log("Kết nối SignalR đã được ngắt."));
-            };
-        }
-    }, [connection, dispatch, userId]);
-    
+  //                         console.log("đã chấp nhận")
+  //                         localStorage.setItem('call',JSON.stringify(message));
+  //                         window.location.href = '/video-call';
+  //                     }
+  //                 });
 
+  //                 connection.on("CancelCall", (message) => {
+  //                     console.log("Nhận tin nhắn từ server:", message);
+  //                     if(message.call.caller === userId){
 
-    return (
-        <React.Fragment>
-           
-            {/* chat left sidebar */}
-            <ChatLeftSidebar recentChatList={users} />
+  //                         console.log("Đã từ chối")
+  //                         localStorage.setItem('call',"");
+  //                         window.location.href = '/';
+  //                     }
+  //                 });
 
-            {/* user chat */}
-            <UserChat recentChatList={users} />
-        </React.Fragment>
-    );
+  //                 connection.on("RequestCall", (message) => {
+  //                     if(message.call.receiver === userId){
+  //                         localStorage.setItem('call',JSON.stringify(message));
+  //                         window.location.href = '/call-pending';
+  //                     }
+  //                 });
+
+  //             })
+  //             .catch((err) => console.error("Kết nối SignalR thất bại:", err));
+
+  //         // Xử lý ngắt kết nối khi component unmount
+  //         return () => {
+  //             connection.stop().then(() => console.log("Kết nối SignalR đã được ngắt."));
+  //         };
+  //     }
+  // }, [connection, dispatch, userId]);
+
+  return (
+    <React.Fragment>
+      {/* chat left sidebar */}
+      <ChatLeftSidebar recentChatList={users} />
+
+      {/* user chat */}
+      <UserChat recentChatList={users} />
+    </React.Fragment>
+  );
 };
 
 export default Index;
