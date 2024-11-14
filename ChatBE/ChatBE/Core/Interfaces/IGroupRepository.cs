0fbe5968c2219fd9d45aab074f5b2b111db7e7ac
duongkz365 <dinhky2468@example.com>
2024-11-14 21:04:01 +0700

    group-be

---
+++ ChatBE/ChatBE/Core/Interfaces/IGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using ChatBE.Core.Entities;
+
+namespace ChatBE.Core.Interfaces
+{
+    public interface IGroupRepository
+    {
+        Task<IEnumerable<Group>> GetAllGroupsAsync();
+        Task<Group> GetGroupByIdAsync(Guid groupId);
+        Task<IEnumerable<Group>> GetGroupByUserIdAsync(Guid userId);
+        Task<string> AddMemberAsync(Guid groupId, Guid userId);
+        Task<string> RemoveMemberAsync(Guid groupId, Guid userId);
+        Task<string> RemoveGroupAsync(Guid groupId);
+        Task<string> AddGroupAsync(Group group);
+
+    }
+}
