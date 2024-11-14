ecab93fe8005a906f1a161f47badf72a07eda39b
duongkz365 <dinhky2468@example.com>
2024-11-14 21:04:01 +0700

    group-be

---
+++ ChatBE/ChatBE/Application/DTOs/GroupDTO/GroupDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.Json.Serialization;
+namespace ChatBE.Application.DTOs.GroupDTO
+{
+    public class GroupDTO
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("members")]
+        public List<Guid> Members { get; set; }
+        [JsonPropertyName("ownerId")]
+        public Guid OwnerId { get; set; }
+    }
+}
